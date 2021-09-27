a4d0c09fee5f582e5a8280e2587204f746023f76
picobyte <PIc.O.0ob.y.TE@gmail.com>
2021-09-27 23:41:19 +0200

    use RVRE as submodule in EditButton
    Signed-off-by: picobyte <PIc.O.0ob.y.TE@gmail.com>

--- game/bypass_main_menu.rpy
+++
@@ -1,6 +0,0 @@
-
-# bypass main menu and quit confirmation
-label main_menu:
-    $ config.quit_action = Quit(confirm=False)
-    return
-
--- game/visual_runtime_editor/__init__.py
+++
@@ -1,6 +0,0 @@
-""" Visual Runtime Editor Module """
-from .buffer import ReadWriteBuffer
-from .renpy_lexer import RenPyLexer
-from .renpyformatter import RenPyFormatter
-
-__all__ = ["ReadWriteBuffer", "RenPyLexer", "RenPyFormatter"]
